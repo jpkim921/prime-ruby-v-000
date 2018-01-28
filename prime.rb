16c2b90b38414fe8946ef4da462fe8a6f13c0903
Jayon Kim <jpkim921@gmail.com>
2018-01-28 22:20:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,29 +1,29 @@
 require 'benchmark'
 
 
-# def prime?(number)
-#   return false if number <= 1
-#
-#   i = 2
-#   while i < number/2
-#     if (number % i) == 0
-#       return false
-#     end
-#     i += 1
-#   end
-#
-#   return true
-# end
+def prime?(number)
+  return false if number <= 1
+
+  i = 2
+  while i < number/2
+    if (number % i) == 0
+      return false
+    end
+    i += 1
+  end
+
+  return true
+end
 
 
 
 # benchmark time = 0.000000   0.000000   0.000000 (  0.000017)
 
-def prime?(number)
-  return false if number <= 1
-  # return true if number / 2 == 1
-
-  !(2..number/2).any? {|x| number % x == 0}
-end
+# def prime?(number)
+#   return false if number <= 1
+#   # return true if number / 2 == 1
+# 
+#   !(2..number/2).any? {|x| number % x == 0}
+# end
 
 puts Benchmark.measure{prime?(7)}
